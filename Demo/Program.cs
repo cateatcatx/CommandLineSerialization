44748420b4ec91f32b795a22b56c74baae91fa79
harveyjin <harveyjin@tencent.com>
2021-12-31 23:21:44 +0800

    Demo update

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -2,19 +2,25 @@
 using System.Collections.Generic;
 using Decoherence.CommandLineSerialization;
 
-var commandline = "--aaa=111 -bca123";
-
-CommandLineDeserializer deserializer = new();
-
-Specs specs = new();
-specs.AddOption(new Option("aaa", OptionValueType.Single, typeof(int)));
-specs.AddOption(new Option('a', OptionValueType.Single, typeof(int)));
-
-var remainArgs =deserializer.Deserialize(commandline.Split(' '), specs, 
-    (option, obj) => Console.WriteLine($"{option.Name} = {obj}"), 
-    null);
-
-foreach (var arg in remainArgs)
+class Program
 {
-    Console.Write($"{arg} ");
+    static void Main(string[] args)
+    {
+        var commandline = "--aaa=111 -bca123";
+
+        CommandLineDeserializer deserializer = new();
+
+        Specs specs = new();
+        specs.AddOption(new Option("aaa", OptionValueType.Single, typeof(int)));
+        specs.AddOption(new Option('a', OptionValueType.Single, typeof(int)));
+
+        var remainArgs =deserializer.Deserialize(commandline.Split(' '), specs, 
+            (option, obj) => Console.WriteLine($"{option.Name} = {obj}"), 
+            null);
+
+        foreach (var arg in remainArgs)
+        {
+            Console.Write($"{arg} ");
+        }
+    }
 }
