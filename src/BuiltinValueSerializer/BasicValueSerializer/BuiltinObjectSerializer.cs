edaf20b89bd7fd443203f0afd4aefdcadfad26af
harveyjin <harveyjin@tencent.com>
2022-01-01 12:27:14 +0800

    c

---
+++ src/BuiltinValueSerializer/BasicValueSerializer/BuiltinObjectSerializer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Decoherence.CommandLineSerialization
+{
+    public class BuiltinObjectSerializer : IValueSerializer
+    {
+        public object? DeserializeNoneValue(Type objType)
+        {
+            throw new InvalidOperationException();
+        }
+
+        public object? DeserializeSingleValue(Type objType, string? value)
+        {
+            throw new InvalidOperationException();
+        }
+
+        public object? DeserializeMultiValue(Type objType, List<string> values)
+        {
+            throw new InvalidOperationException();
+        }
+
+        public object? DeserializeWhenNoMatch(Type objType)
+        {
+            return null;
+        }
+    }
+}
